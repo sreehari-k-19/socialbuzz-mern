f8928546891d95a7a1408b0e1f718775e14823ef
sreeharik <sreehari9188@gmail.com>
2023-04-10 22:47:48 -0500

    adminstart

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors({origin:[
     "https://socialbuzz.fun",
+    "http://socialbuzz.fun",
     "http://localhost:3000",
     "https://api.socialbuzz.fun",
     "*"
